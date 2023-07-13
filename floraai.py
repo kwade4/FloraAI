fbea5768e5ac4e8e20f77cdc5595650f123f0af2
kwade4 <kwade4@uwo.ca>
2023-07-12 22:47:26 -0400

    -Setting up model and training loop (#6)

--- floraai.py
+++ floraai.py
@@ -3,8 +3,10 @@ Load Data for 102 Categories of Flowers
 """
 
 import torch
-from torchvision import transforms, datasets
+from torchvision import transforms, datasets, models
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import torchmetrics
 import matplotlib.pyplot as plt
 import os
 
@@ -63,7 +65,7 @@ def load_training_dataset(data_path):
 
     # Get batches of images (generator)
     training_data_loader = torch.utils.data.DataLoader(training_data,
-                                                       batch_size=32,
+                                                       batch_size=48,
                                                        num_workers=os.cpu_count(),
                                                        shuffle=True)
 
@@ -89,14 +91,15 @@ def load_validation_dataset(val_path):
     validation_data_loader = torch.utils.data.DataLoader(validation_data,
                                                          batch_size=32,
                                                          num_workers=os.cpu_count(),
-                                                         shuffle=True)
+                                                         shuffle=False,
+                                                         drop_last=False)
     return validation_data_loader
 
 
 def display_image(images):
     """
-    Display images in dataset
-    :param images: the image
+    Display first image of the batch
+    :param images: the batch of images
     :return: None
     """
     images_np = images.numpy()
@@ -117,7 +120,92 @@ def calculate_mean_std(img_loader):
     return mean, std
 
 
+def train():
+    train_loader, training_classes = load_training_dataset(TRAIN_PATH)
+    validation_loader = load_validation_dataset(VALIDATION_PATH)
+
+    # Setting up network
+    num_classes = len(training_classes)
+    net = models.efficientnet_b0(num_classes=num_classes).cuda()
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+    criterion = torch.nn.CrossEntropyLoss()
+    num_epoch = 500
+    num_batches = len(train_loader)
+
+    # Set up tensorboard writer
+    writer = SummaryWriter()
+    step = 1
+
+    # Set up validation metrics
+    acc_metric = torchmetrics.classification.MulticlassAccuracy(num_classes=num_classes).cuda()
+    f1_metric = torchmetrics.classification.MulticlassF1Score(num_classes=num_classes).cuda()
+    recall_metric = torchmetrics.classification.MulticlassRecall(num_classes=num_classes).cuda()
+    precision_metric = torchmetrics.classification.MulticlassPrecision(num_classes=num_classes).cuda()
+    auroc_metric = torchmetrics.classification.MulticlassAUROC(num_classes=num_classes).cuda()
+
+
+    for i in range(num_epoch):
+        epoch_loss = 0
+        batch_mean = torchmetrics.aggregation.MeanMetric().cuda()
+        for idx, (images, label) in enumerate(train_loader):
+            optimizer.zero_grad()
+
+            images = images.cuda()
+            label = label.cuda()
+
+            # Training and calculating loss
+            label_pred = net(images)
+            loss = criterion(input=label_pred, target=label)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+
+            print("Epoch {}, Iteration {} of {}, Batch loss: {}".format(i+1, idx+1, num_batches, loss.item()))
+            writer.add_scalar("Train/Loss", loss.item(), step)
+            batch_mean(acc_metric(torch.nn.functional.softmax(label_pred, dim=1).argmax(dim=1), label))
+            step += 1
+
+        train_acc = batch_mean.compute().item()
+        writer.add_scalar("Train/Accuracy", train_acc, i+1)
+
+        print('Finished Epoch {}, Training accuracy {}'.format(i+1, train_acc))
+
+        # Calculate validation stats after every epoch
+        with torch.no_grad():
+
+            label_list = []
+            pred_list = []
+
+            for images, label in validation_loader:
+                images = images.cuda()
+                label = label.cuda()
+
+                label_pred = torch.nn.functional.softmax(net(images), dim=1)
+
+                label_list.append(label)
+                pred_list.append(label_pred)
+
+            labels = torch.cat(label_list)
+            preds =  torch.cat(pred_list)
+
+            acc = acc_metric(preds, labels)
+            f1_score = f1_metric(preds, labels)
+            recall = recall_metric(preds, labels)
+            precision = precision_metric(preds, labels)
+            auroc = auroc_metric(preds, labels)
+
+            writer.add_scalar('Validation/Accuracy', acc.item(), i+1)
+            writer.add_scalar('Validation/F1 Score', f1_score.item(), i + 1)
+            writer.add_scalar('Validation/Recall', recall.item(), i + 1)
+            writer.add_scalar('Validation/Precision', precision.item(), i + 1)
+            writer.add_scalar('Validation/AUROC', auroc.item(), i + 1)
+
+    torch.save(net, os.path.join(writer.log_dir, 'model.pt'))
+
+
 if __name__ == "__main__":
-    image_data_loader, class_to_idx = load_training_dataset(TRAIN_PATH)
-    images, label = next(iter(image_data_loader))
-    display_image(images)
+    # image_data_loader, class_to_idx = load_training_dataset(TRAIN_PATH)
+    # images, label = next(iter(image_data_loader))
+    # display_image(images)
+
+    train()
